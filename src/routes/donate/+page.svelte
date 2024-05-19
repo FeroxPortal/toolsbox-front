11cc239f7420228605528a99d974f1648ce698cc
pixofhell <pixofheaven@gmail.com>
2024-05-19 18:41:48 +0200

    ajout meta

--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -2,7 +2,14 @@
 
     import HomeLink from '../../lib/components/HomeLink.svelte';
 
+    const siteName = "ToolsBox 4 DEV"
+
 </script>
+
+<svelte:head>
+	<title>{siteName} - Paypal donation link</title>
+	<meta name="description" content="{siteName} - Paypal donation link">
+</svelte:head>
 
 <HomeLink />
 <h1>
